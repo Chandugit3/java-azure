423a80c204f61994ad76d9f6e9318545546c00bf
chandu <chandubanoth980@gmail.com>
2021-10-25 12:22:12 +0530

    first

--- src/main/java/com/javatechie/spring/azure/SpringAzureApplication.java
+++ src/main/java/com/javatechie/spring/azure/SpringAzureApplication.java
@@ -2,9 +2,17 @@ package com.javatechie.spring.azure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAzureApplication {
+	
+	@GetMapping("/message")
+	public String message() {
+		return "welcome to azure";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureApplication.class, args);
